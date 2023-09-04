f296e5b6f1d182b2dfdd1f1908cf2909d48fa80c
Muhammad Agung Ferdiansyah <muhammad_agungf@student.ub.ac.id>
2023-09-04 07:57:32 +0700

    fixing

--- go.mod
+++ go.mod
@@ -8,14 +8,27 @@ require (
 )
 
 require (
+	gioui.org v0.2.0 // indirect
+	gioui.org/cpu v0.0.0-20220412190645-f1e9e8c3b1f7 // indirect
+	gioui.org/shader v1.0.6 // indirect
+	gioui.org/x v0.2.0 // indirect
+	git.sr.ht/~sbinet/gg v0.5.0 // indirect
+	github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b // indirect
+	github.com/andybalholm/stroke v0.0.0-20221221101821-bd29b49d73f0 // indirect
 	github.com/bytedance/sonic v1.9.1 // indirect
+	github.com/campoy/embedmd v1.0.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-fonts/liberation v0.3.1 // indirect
+	github.com/go-latex/latex v0.0.0-20230307184459-12ec69307ad9 // indirect
+	github.com/go-pdf/fpdf v0.8.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
+	github.com/go-text/typesetting v0.0.0-20230803102845-24e03d8b5372 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
@@ -23,13 +36,21 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
+	github.com/wcharczuk/go-chart v2.0.1+incompatible // indirect
+	github.com/wcharczuk/go-chart/v2 v2.1.1 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/exp v0.0.0-20230801115018-d63ba01acd4b // indirect
+	golang.org/x/exp/shiny v0.0.0-20230801115018-d63ba01acd4b // indirect
+	golang.org/x/image v0.11.0 // indirect
 	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
+	gonum.org/v1/plot v0.13.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	rsc.io/pdf v0.1.1 // indirect
 )
--- main.go
+++ main.go
@@ -7,213 +7,275 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/wcharczuk/go-chart"
 )
 
-type Transaction struct {
-	ID              int
-	ItemDescription string
-	ItemCost        int
-	AddBalance      int
-	BalanceAfter    int
+type StockItem struct {
+	ID          int
+	StockID     int
+	Dates       time.Time
+	CoffeeType  string
+	Pack1       int
+	Pack2       int
+	Pack3       int
+	Amount1     int
+	Amount2     int
+	Amount3     int
+	TotalAmount int
 }
 
-var tmpl = template.Must(template.ParseGlob("templates/*"))
+var tmpl = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
-	// ... (other parts of your main function)
-
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/create", createHandler)
-	http.HandleFunc("/update", updateHandler)
-	http.HandleFunc("/delete", deleteHandler)
-	// Serve static assets from the "assets" directory
-	fs := http.FileServer(http.Dir("assets"))
-
-	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
-
-	http.ListenAndServe("localhost:9990", nil)
-}
-
-// ... (indexHandler, updateHandler, and deleteHandler functions here)
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/balance")
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/wonosantri")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	rows, err := db.Query("SELECT transactionid, item_description, item_cost, add_balance, balance_after FROM accounting")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-
-	var transactions []Transaction
-	for rows.Next() {
-		var t Transaction
-		err := rows.Scan(&t.ID, &t.ItemDescription, &t.ItemCost, &t.AddBalance, &t.BalanceAfter)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.Query("SELECT stockid, dates, coffeetype, pack1, pack2, pack3, amount1, amount2, amount3, totalamount FROM wonosantristock")
 		if err != nil {
 			log.Fatal(err)
 		}
-		transactions = append(transactions, t)
-	}
+		defer rows.Close()
 
-	if err = rows.Err(); err != nil {
-		log.Fatal(err)
-	}
+		var stockItems []StockItem
+		for rows.Next() {
+			var stock StockItem
+			var dateString string
+			err := rows.Scan(
+				&stock.ID,
+				&dateString,
+				&stock.CoffeeType,
+				&stock.Pack1,
+				&stock.Pack2,
+				&stock.Pack3,
+				&stock.Amount1,
+				&stock.Amount2,
+				&stock.Amount3,
+				&stock.TotalAmount,
+			)
+			if err != nil {
+				log.Fatal(err)
+			}
 
-	tmpl.ExecuteTemplate(w, "index.html", transactions)
+			stock.Dates, err = time.Parse("2006-01-02 15:04:05", dateString)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			stockItems = append(stockItems, stock)
+		}
+
+		err = tmpl.ExecuteTemplate(w, "index.html", stockItems)
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+
+	http.HandleFunc("/create", createHandler)
+	http.HandleFunc("/update", updateHandler)
+	http.HandleFunc("/delete", deleteHandler)
+	http.HandleFunc("/summary", summaryHandler)
+
+	fs := http.FileServer(http.Dir("assets"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
+	fmt.Println("Server started at :7000")
+	http.ListenAndServe(":7000", nil)
+
 }
 
 func createHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
-		db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/balance")
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer db.Close()
+		dates := r.FormValue("dates")
+		coffeetype := r.FormValue("coffeetype")
+		pack1, _ := strconv.Atoi(r.FormValue("pack1"))
+		pack2, _ := strconv.Atoi(r.FormValue("pack2"))
+		pack3, _ := strconv.Atoi(r.FormValue("pack3"))
 
-		err = db.Ping()
-		if err != nil {
-			log.Fatal(err)
-		}
+		// Calculate temporary Amount1, Amount2, Amount3, and TotalAmount for review
+		amount1 := pack1 * 200
+		amount2 := pack2 * 500
+		amount3 := pack3 * 1000
+		totalamount := amount1 + amount2 + amount3
 
-		r.ParseForm()
-		itemDescription := r.FormValue("item_description")
-		itemCost := r.FormValue("item_cost")
-		addBalance := r.FormValue("add_balance")
-		balanceAfter := r.FormValue("balance_after")
+		if r.FormValue("final") == "true" {
+			db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/wonosantri")
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			defer db.Close()
 
-		// Convert form values to appropriate data types
-		itemCostInt, err := strconv.Atoi(itemCost)
-		if err != nil {
-			log.Fatal(err)
-		}
-		addBalanceInt, err := strconv.Atoi(addBalance)
-		if err != nil {
-			log.Fatal(err)
-		}
-		balanceAfterInt, err := strconv.Atoi(balanceAfter)
-		if err != nil {
-			log.Fatal(err)
-		}
+			// Insert the data into the database
+			insertQuery := "INSERT INTO wonosantristock (dates, coffeetype, pack1, pack2, pack3, amount1, amount2, amount3, totalamount) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+			_, err = db.Exec(insertQuery, dates, coffeetype, pack1, pack2, pack3, amount1, amount2, amount3, totalamount)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
 
-		// Perform INSERT operation using the converted values
-		insertQuery := "INSERT INTO accounting (item_description, item_cost, add_balance, balance_after) VALUES (?, ?, ?, ?)"
-		_, err = db.Exec(insertQuery, itemDescription, itemCostInt, addBalanceInt, balanceAfterInt)
-		if err != nil {
-			log.Fatal(err)
-		}
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+		} else {
+			reviewData := struct {
+				Dates       string
+				CoffeeType  string
+				Pack1       int
+				Pack2       int
+				Pack3       int
+				Amount1     int
+				Amount2     int
+				Amount3     int
+				TotalAmount int
+			}{
+				Dates:       dates,
+				CoffeeType:  coffeetype,
+				Pack1:       pack1,
+				Pack2:       pack2,
+				Pack3:       pack3,
+				Amount1:     amount1,
+				Amount2:     amount2,
+				Amount3:     amount3,
+				TotalAmount: totalamount,
+			}
 
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-		return
+			data := struct {
+				Review interface{}
+			}{
+				Review: reviewData,
+			}
+
+			tmpl.ExecuteTemplate(w, "create.html", data)
+		}
+	} else {
+		tmpl.ExecuteTemplate(w, "create.html", nil)
 	}
-
-	tmpl.ExecuteTemplate(w, "create.html", nil)
 }
+
 func updateHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		// Get the transaction ID from the query parameter
-		transactionID := r.URL.Query().Get("transaction_id")
-
-		// Convert the transaction ID to an integer
-		transactionIDInt, err := strconv.Atoi(transactionID)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// Fetch the transaction data from the database
-		db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/balance")
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer db.Close()
-
-		query := "SELECT transactionid, item_description, item_cost, add_balance, balance_after FROM accounting WHERE transactionid = ?"
-		row := db.QueryRow(query, transactionIDInt)
-
-		var t Transaction
-		err = row.Scan(&t.ID, &t.ItemDescription, &t.ItemCost, &t.AddBalance, &t.BalanceAfter)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		tmpl.ExecuteTemplate(w, "update.html", t)
-		return
-	}
-
+	// Check if the request method is POST (form submission)
 	if r.Method == http.MethodPost {
-		// Parse form values
-		r.ParseForm()
-		transactionID := r.FormValue("transaction_id")
-		itemDescription := r.FormValue("item_description")
-		itemCost := r.FormValue("item_cost")
-		addBalance := r.FormValue("add_balance")
-		balanceAfter := r.FormValue("balance_after")
-
-		// Convert form values to appropriate data types
-		transactionIDInt, err := strconv.Atoi(transactionID)
+		// Parse the form data
+		err := r.ParseForm()
 		if err != nil {
-			log.Fatal(err)
-		}
-		itemCostInt, err := strconv.Atoi(itemCost)
-		if err != nil {
-			log.Fatal(err)
-		}
-		addBalanceInt, err := strconv.Atoi(addBalance)
-		if err != nil {
-			log.Fatal(err)
-		}
-		balanceAfterInt, err := strconv.Atoi(balanceAfter)
-		if err != nil {
-			log.Fatal(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 
-		// Perform the update operation using the extracted values
-		db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/balance")
+		// Retrieve the form values
+		stockID, _ := strconv.Atoi(r.FormValue("stockid"))
+		dates := r.FormValue("dates")
+		coffeetype := r.FormValue("coffeetype")
+		pack1, _ := strconv.Atoi(r.FormValue("pack1"))
+		pack2, _ := strconv.Atoi(r.FormValue("pack2"))
+		pack3, _ := strconv.Atoi(r.FormValue("pack3"))
+		// Add lines to retrieve the additional fields
+		amount1, _ := strconv.Atoi(r.FormValue("amount1"))
+		amount2, _ := strconv.Atoi(r.FormValue("amount2"))
+		amount3, _ := strconv.Atoi(r.FormValue("amount3"))
+		totalamount, _ := strconv.Atoi(r.FormValue("totalamount"))
+
+		// Update the stock item in the database
+		db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/wonosantri")
 		if err != nil {
-			log.Fatal(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 		defer db.Close()
 
-		updateQuery := "UPDATE accounting SET item_description = ?, item_cost = ?, add_balance = ?, balance_after = ? WHERE transactionid = ?"
-		_, err = db.Exec(updateQuery, itemDescription, itemCostInt, addBalanceInt, balanceAfterInt, transactionIDInt)
+		updateQuery := "UPDATE wonosantristock SET dates=?, coffeetype=?, pack1=?, pack2=?, pack3=?, amount1=?, amount2=?, amount3=?, totalamount=? WHERE stockid=?"
+		_, err = db.Exec(updateQuery, dates, coffeetype, pack1, pack2, pack3, amount1, amount2, amount3, totalamount, stockID)
 		if err != nil {
-			log.Fatal(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 
-		fmt.Println("Updating transaction:", transactionID)
-		// Redirect or display a success message
+		// Redirect the user back to the main page
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
 
-	tmpl.ExecuteTemplate(w, "update.html", nil)
+	// If the request method is not POST (e.g., GET), retrieve the stockid from the URL parameters
+	stockID, err := strconv.Atoi(r.URL.Query().Get("stockid"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Retrieve the existing data for the selected stock item based on stockID
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/wonosantri")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	row := db.QueryRow("SELECT dates, coffeetype, pack1, pack2, pack3, amount1, amount2, amount3, totalamount FROM wonosantristock WHERE stockid=?", stockID)
+
+	var dates, coffeetype string
+	var pack1, pack2, pack3, amount1, amount2, amount3, totalamount int
+
+	err = row.Scan(&dates, &coffeetype, &pack1, &pack2, &pack3, &amount1, &amount2, &amount3, &totalamount)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Create a struct to store the retrieved data
+	stockItem := struct {
+		StockID     int
+		Dates       string
+		CoffeeType  string
+		Pack1       int
+		Pack2       int
+		Pack3       int
+		Amount1     int
+		Amount2     int
+		Amount3     int
+		TotalAmount int
+	}{
+		StockID:     stockID,
+		Dates:       dates,
+		CoffeeType:  coffeetype,
+		Pack1:       pack1,
+		Pack2:       pack2,
+		Pack3:       pack3,
+		Amount1:     amount1,
+		Amount2:     amount2,
+		Amount3:     amount3,
+		TotalAmount: totalamount,
+	}
+
+	// Execute the update template and pass the data
+	tmpl.ExecuteTemplate(w, "update.html", stockItem)
 }
 
 func deleteHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		// Parse form values
 		r.ParseForm()
-		transactionID := r.FormValue("transaction_id")
+		stockID := r.FormValue("stockid")
 
 		// Convert form values to appropriate data types
-		transactionIDInt, err := strconv.Atoi(transactionID)
+		stockIDInt, err := strconv.Atoi(stockID)
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		// Perform DELETE operation using the converted value
-		db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/balance")
+		db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/wonosantri")
 		if err != nil {
 			log.Fatal(err)
 		}
 		defer db.Close()
 
-		deleteQuery := "DELETE FROM accounting WHERE transactionid = ?"
-		_, err = db.Exec(deleteQuery, transactionIDInt)
+		deleteQuery := "DELETE FROM wonosantristock WHERE stockid = ?"
+		_, err = db.Exec(deleteQuery, stockIDInt)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -223,4 +285,45 @@ func deleteHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	tmpl.ExecuteTemplate(w, "delete.html", nil)
+}
+func summaryHandler(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/wonosantri")
+	if err != nil {
+		log.Fatal(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	// Calculate the total amounts of Arabica and Robusta coffee
+	var arabicaTotal int
+	var robustaTotal int
+
+	// Query your database to get the total amounts
+	query := `
+        SELECT 
+            SUM(COALESCE(CASE WHEN coffeetype = 'Arabica' THEN totalamount ELSE 0 END, 0)) AS arabica_total,
+            SUM(COALESCE(CASE WHEN coffeetype = 'Robusta' THEN totalamount ELSE 0 END, 0)) AS robusta_total
+        FROM wonosantristock
+        WHERE coffeetype IN ('Arabica', 'Robusta');
+    `
+
+	err = db.QueryRow(query).Scan(&arabicaTotal, &robustaTotal)
+	if err != nil {
+		log.Fatal(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Pass these values to the template
+	data := struct {
+		ArabicaTotal int
+		RobustaTotal int
+	}{
+		ArabicaTotal: arabicaTotal,
+		RobustaTotal: robustaTotal,
+	}
+
+	// Render the summary.html template with the data
+	tmpl.ExecuteTemplate(w, "summary.html", data)
 }
